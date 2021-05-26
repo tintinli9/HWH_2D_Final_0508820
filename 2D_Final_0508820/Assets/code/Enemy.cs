618e361f9fdb169d965ab44866ea8fe9df6ef18c
tintinli9 <71861182+tintinli9@users.noreply.github.com>
2021-05-27 02:27:22 +0800

    敵人

---
+++ 2D_Final_0508820/Assets/code/Enemy.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("追蹤範圍"), Range(0, 500)]
+    public float rangeTrack = 4;
+    [Header("攻擊範圍"), Range(0, 50)]
+    public float rangeAttack = 2;
+    [Header("移動速度"), Range(0, 15)]
+    public float speed = 2;
+    [Header("攻擊動畫")]
+    public Animator ani;
+    [Header("攻擊冷卻時間"), Range(0, 10)]
+    public float cdAttack = 3;
+    [Header("攻擊力"), Range(0, 100)]
+    public float attack = 20;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("攻擊音效")]
+    public AudioClip soundAttack;
+
+    private Transform player;
+    private float timer;
+
+    private void Start()
+    {
+        player = GameObject.Find("玩家").transform;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.5f);
+        Gizmos.DrawSphere(transform.position, rangeTrack);
+
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        float dis = Vector3.Distance(transform.position, player.position);
+
+        if (dis <= rangeAttack)
+        {
+            Attack();
+        }
+        else if (dis <= rangeTrack)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    public void Attack()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= cdAttack)
+        {
+            timer = 0;
+            aud.PlayOneShot(soundAttack, 0.3f);
+        }
+        
+
+    }
+}
---
+++ 2D_Final_0508820/Assets/code/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int lv = 1;
+    [Range(0, 15)]
+    public float speed = 10.5f;
+    public bool isDEAD = false;
+    public string cNAME = "主角";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
+    [Header("偵測範圍")]
+    public float rangeAttack = 1.5f;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("攻擊音效")]
+    public AudioClip soundAttack;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.1f);
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+
+    private void Move()
+    {
+        float h = joystick.Horizontal;
+
+        tra.Translate(h * speed * Time.deltaTime, 0, 0);
+
+        ani.SetFloat("水平", h);
+    }
+
+    public void Attack()
+    {
+        aud.PlayOneShot(soundAttack, 0.3f);
+
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, -transform.up, 0, 1 << 8);
+    }
+
+    private void Hit()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+}
+
